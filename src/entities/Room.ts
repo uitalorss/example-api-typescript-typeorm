251e37b6d91862ed51a19baaca2ed658e2d80ea9
Uítalo <uitalo.urss@gmail.com>
2023-10-21 22:17:33 -0300

    configuração da migration das tabelas videos e subject_videos

--- src/entities/Room.ts
+++ src/entities/Room.ts
@@ -1,5 +1,12 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  Entity,
+  ManyToMany,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
 import { Video } from "./Video";
+import { Subject } from "./Subject";
 
 @Entity("rooms")
 export class Room {
@@ -9,4 +16,6 @@ export class Room {
   name: string;
   @OneToMany(() => Video, (video) => video.room)
   videos: Video[];
+  @ManyToMany(() => Subject, (subject) => subject.rooms)
+  subjects: Subject[];
 }
