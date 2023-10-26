58a6272136d48d937d3de192340d21b8ae74803f
Uítalo <uitalo.urss@gmail.com>
2023-10-26 16:10:44 -0300

    estrutura para tratamento de erros

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
-import express from "express";
+import "express-async-errors";
+import express, { NextFunction, Request, Response } from "express";
 import { AppDataSource } from "./data-source";
 import routes from "./routes";
+import { ApiError } from "./helpers/api-errors";
 
 AppDataSource.initialize().then(() => {
   const app = express();
 
   app.use(express.json());
   app.use(routes);
+
+  app.use(
+    (
+      error: Error & Partial<ApiError>,
+      req: Request,
+      res: Response,
+      next: NextFunction
+    ) => {
+      const statusCode = error.statusCode ?? 500;
+      const message = error.statusCode
+        ? error.message
+        : "Internal Server Error.";
+      return res.status(statusCode).json({ message: message });
+    }
+  );
 
   return app.listen(process.env.PORT);
 });
--- src/routes.ts
+++ src/routes.ts
@@ -3,8 +3,13 @@ import { SubjectController } from "./controllers/SubjectController";
 import { RoomController } from "./controllers/RoomController";
 import { videoController } from "./controllers/videoController";
 import { roomRepository } from "./repositories/RoomRepository";
+import { ApiError, BadRequestError } from "./helpers/api-errors";
 
 const routes = Router();
+
+routes.get("/", async (req, res) => {
+  throw new BadRequestError("Caiu no erro.");
+});
 
 routes.post("/subject", new SubjectController().create);
 routes.post("/room", new RoomController().create);
